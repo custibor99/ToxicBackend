5fa711370ce71b006ce9236637bce38296ed87b2
Tibor Cus <cus.tibor@outlook.com>
2024-10-07 08:11:55 +0200

    refactor files

--- toxicbygnn/models/classicalModels.py
+++ toxicbygnn/models/classicalModels.py
@@ -1,7 +1,41 @@
-from toxicbygnn.models.BaseModel import BaseModel
 import numpy as np
 from pathlib import Path
 import random as rnd
+
+class BaseModel:
+    def checkInput(self, X: np.ndarray):
+        if not isinstance(X, np.ndarray):
+            raise TypeError(f"Incorrect type of X. Is of type {type(X)} not ndarray")
+        ndim = len(X.shape)
+        if ndim != 2:
+            raise ValueError(f"Incorrect number of dimensions. Expected 2 got {ndim}")
+        n, m = X.shape
+        if n != 1:
+            raise ValueError(f"incorrect shape: {X.shape}")
+        
+    def checkBatchInput(self, X: np.ndarray):
+        if not isinstance(X, np.ndarray):
+            raise TypeError(f"Incorrect type of X. Is of type {type(X)} not ndarray")
+        ndim = len(X.shape)
+        if ndim != 2:
+            raise ValueError(f"Incorrect number of dimensions. Expected 2 got {ndim}")
+
+    def predict(self, X: np.ndarray) -> float:
+        raise NotImplementedError()
+    
+    def predictBatch(self, X: np.ndarray) -> list[float]:
+        raise NotImplementedError()
+
+    def save(self, filepath: Path):
+        raise NotImplementedError()
+    
+    def load(self, filepath: Path):
+        raise NotImplementedError()
+    
+    def __str__(self, ):
+        raise NotImplementedError()
+    
+
 
 class RandomModel(BaseModel):
     def __init__(self, seed = None):
