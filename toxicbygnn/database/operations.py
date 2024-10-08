6490252e9e16977cf267a48b4953f9526eeb49df
Tibor Cus <cus.tibor@outlook.com>
2024-10-08 16:28:50 +0200

    add database operations

---
+++ toxicbygnn/database/operations.py
@@ -0,0 +1,40 @@
+from toxicbygnn.database.schema import Chemical, Model, Endpoint, Prediction
+from toxicbygnn.database import engine
+
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+
+def createAllEntitites(entities: list):
+    with Session(engine) as session:
+        session.add_all(entities)
+        session.commit()
+
+def getAllFromTable(table) -> list:
+    stmt = select(table)
+    with Session(engine) as session:    
+        res = session.scalars(stmt)
+        return res.all()
+
+
+
+
+endpoints = [
+    Endpoint(name="Cancer"),
+    Endpoint(name="Sperm")
+]
+
+
+createAllEntitites(endpoints)
+created = getAllFromTable(Endpoint)
+
+models = [
+    Model(name="RandomModel", endpointId=el.id)
+    for el in created
+]
+createAllEntitites(models)
+model = getAllFromTable(Model)[0]
+print(model, type(model))
+
+
+
